5ea796f59e4258c6d65804de3b9e88b7f70536ef
Niranjan07082014 <54843134+Niranjan07082014@users.noreply.github.com>
2020-02-02 23:13:55 +0530

    changes of front-end design

--- view/examPage/index.php
+++ view/examPage/index.php
@@ -1,9 +1,98 @@
-<div class="right_col" role="main">
-          <div class="row" style="display: inline-block;">
-            <div class=" top_tiles" style="margin: 10px 0;">
-              <div class="col-md-12 col-sm-12 tile">
-                
-              </div>
-            </div>
-         </div>
-      </div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+		</div>
+		<div class="col-md-3">
+		</div>
+		<!-- User Details Section Start -->
+		<div class="col-md-5">
+			<div class="row">
+				<div class="col-md-2">
+					<img src="<?php echo URL;  ?>public/images/OnlineExam.jpg" class="img-fluid img-thumbnail" height="80" width="80">
+				</div>
+				<div class="col-md-10">
+					<div class="row">
+						<div class="col-md-5">
+							<label>Candidate Name :</label>
+						</div>
+						<div class="col-md-5">
+							<span>Niranja Behera</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-5">
+							<label>Subject Name :</label>
+						</div>
+						<div class="col-md-5">
+							<span>Test Practice</span>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-5">
+							<label>Remaining Time :</label>
+						</div>
+						<div class="col-md-5">
+							<span class="bg-primary text-white p-1 rounded">00:53:13</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- User Details Section End -->
+	</div>
+</div>
+<div class="container-fluid bg-warning">
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-lg-12 p-2">
+			<button type="button" class="btn btn-info float-right"><span class="glyphicon glyphicon-download-alt"></span> DOWNLOAD QUESTION</button>
+		</div>
+	</div>
+</div>
+<!-- Question Section Start -->
+<div class="container">
+	<div class="row mt-5">
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-12">
+					<h5>Welcome</h5>
+				</div>				
+			</div>
+		</div>
+		<div class="col-md-4 mt-4">
+			<div class="row">
+				<div class="col-md-6">
+					<span class="bg-secondary rounded p-2 text-white">1</span><span> Not Visited</span>
+				</div>
+				<div class="col-md-6">
+					<span class="bg-danger rounded p-2 text-white">2</span><span> Not Answere</span>
+				</div>
+			</div>
+			<div class="row mt-4">
+				<div class="col-md-6">
+					<span class="bg-secondary rounded p-2 text-white">0</span><span> Answere</span>
+				</div>
+				<div class="col-md-6">
+					<span class="bg-secondary rounded p-2 text-white">0</span><span> Marked for Review</span>
+				</div>
+			</div>
+			<div class="row mt-4 mb-5">
+				<div class="col-md-12 m">
+					<span class="bg-secondary rounded p-2 text-white">0</span><span> Answere & Marked for Review (will be considered for evaluation</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<span class="bg-secondary rounded p-3 text-white">01</span>
+					<span class="bg-secondary rounded p-3 text-white">02</span>
+					<span class="bg-secondary rounded p-3 text-white">03</span>
+					<span class="bg-secondary rounded p-3 text-white">04</span>
+					<span class="bg-secondary rounded p-3 text-white">05</span>
+					<span class="bg-secondary rounded p-3 text-white">06</span>
+					<span class="bg-secondary rounded p-3 text-white">07</span>
+					<span class="bg-secondary rounded p-3 text-white">08</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Question Section End -->
